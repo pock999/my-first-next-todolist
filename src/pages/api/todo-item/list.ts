d9337908f2e4482d4a7d74a803594c01bfe6a44b
Rickh <hpx210650@gmail.com>
2024-12-07 01:36:10 +0800

    ✨ feat: list api add keyword filter

--- src/pages/api/todo-item/list.ts
+++ src/pages/api/todo-item/list.ts
@@ -1,3 +1,4 @@
+import { WhereClauseCondition } from './../../../../node_modules/typeorm/query-builder/WhereClause.d';
 import { getDBConnection } from '@/data-source';
 import { IResponse } from '@/dtos/interfaces/response.interface';
 import { TodoItemCreateDto } from '@/dtos/req/todo-item-create.dto';
@@ -5,18 +6,31 @@ import { TodoListDto } from '@/dtos/res/todo-list.dto';
 import { TodoItemEntity } from '@/entitys/todo-item.entity';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import ResponseUtil from '@/utils/response.util';
+import { FindManyOptions } from 'typeorm';
+import { Or, Like } from 'typeorm';
+
+interface ListFilterRequest extends NextApiRequest {
+  query: {
+    q: string; // 關鍵字
+  };
+}
 
 /**
  * @swagger
  * /api/todo-item/list:
  *   get:
  *     description: Returns Todo List
+ *     parameters:
+ *       - in: query
+ *         name: q
+ *         type: string
+ *         description: The Keyword
  *     responses:
  *       200:
  *         description: return Todo List
  */
 export default async function handler(
-  req: NextApiRequest,
+  req: ListFilterRequest,
   res: NextApiResponse<IResponse<any>>
 ) {
   const { method } = req;
@@ -28,6 +42,23 @@ export default async function handler(
     return;
   }
 
-  const result = await conn.getRepository(TodoItemEntity).find();
+  // keyword
+  const { q } = req.query;
+
+  let optionClauseCondition: FindManyOptions = {};
+
+  if (!!q) {
+    optionClauseCondition = {
+      where: [{ title: Like(`%${q}%`) }, { content: Like(`%${q}%`) }],
+    };
+  }
+
+  optionClauseCondition.order = {
+    seq: 'ASC',
+  };
+
+  const result = await conn
+    .getRepository(TodoItemEntity)
+    .find(optionClauseCondition);
   res.status(200).json(ResponseUtil.ok(result));
 }
