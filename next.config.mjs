c89d3a93f7841c06911dd1484d6432f66df37207
Rickh <hpx210650@gmail.com>
2024-12-08 00:31:35 +0800

    ✨ feat: add CORS, getServerSideProps, sortablejs

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   rules: [
-    { 
+    {
       test: /\.(ts|tsx)$/,
       use: 'babel-loader',
-      exclude: /node_modules/
-    }
-  ]
+      exclude: /node_modules/,
+    },
+  ],
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,104 +1,61 @@
-import Image from "next/image";
+import { TodoListDto } from "@/dtos/res/todo-list.dto";
+import { TodoItemEntity } from "@/entitys/todo-item.entity";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import { ReactSortable } from "react-sortablejs";
 
+export default function Home(props: { data: Array<TodoItemEntity> }) {
 
-export default function Home() {
+  const [q, setQ] = React.useState<string>('');
+  const [todoList, setTodoList] = React.useState<Array<TodoItemEntity>>([]);
+
+  React.useEffect(() => {
+    setTodoList(props.data);
+  }, []);
+
   return (
     <div
-      className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`grid grid-rows-[100px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+      <div className={`flex-col flex grow w-full`}>
+        <label
+          htmlFor={"key_word"}
+          className={"flex grow items-center mb-2 text-sm font-medium text-gray-900 dark:text-white"}>
+            Keyword (Title/Content)
+        </label>
+        <input
+          type="text"
+          id="key_word"
+          className={"flex grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "}
+          placeholder="Keyword"
+          required
+          value={q}
+          onChange={evt => setQ(evt.target.value)}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <ReactSortable list={todoList} setList={(e) => {
+        setTodoList(e);
+        console.log('e => ', e);
+      }}>
+        {
+          todoList.map((item: TodoItemEntity) => (
+            <div key={item.id}> [id: {item.id}, seq: {item.seq}]client - { item.title }</div>
+          ))
+        }
+      </ReactSortable>
     </div>
   );
+}
+
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  console.log('server side');
+  const apiPath = `${process.env.NEXT_PUBLIC_API_URL}/todo-item/list`
+  const res: TodoListDto = await fetch(apiPath).then(res => res.json());
+
+  return {
+    props: {
+      data: res.result ?? []
+    }
+  };
 }
