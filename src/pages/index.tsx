c4070f12a749dd61b91f07b38cdcb298b32afbc5
Rickh <hpx210650@gmail.com>
2024-12-11 21:20:54 +0800

    ✨ feat: todo item card style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,53 +107,54 @@ export default function Home(props: { data: Array<TodoItemEntity> }) {
             className="relative flex flex-col my-6 bg-white shadow-sm border dark:border-slate-200 border-slate-950 rounded-lg w-full"
           >
             <div className="p-4">
-              <h5
-                className={
-                  'mb-2 text-slate-800 text-xl font-semibold ' +
-                  (item.status === TodoStatusConst.DONE ? 'line-through' : '')
-                }
-              >
-                {item.title}
-              </h5>
+              <div className="flex justify-between">
+                <div className="flex items-center">
+                  <input
+                    checked={item.status === TodoStatusConst.DONE}
+                    id="checked-checkbox"
+                    type="checkbox"
+                    onChange={(evt) => toggleDone(evt, item.id)}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    htmlFor="checked-checkbox"
+                    className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  ></label>
+                  <h5
+                    className={
+                      ' text-slate-800 text-xl font-semibold ' +
+                      (item.status === TodoStatusConst.DONE
+                        ? 'line-through'
+                        : '')
+                    }
+                  >
+                    {item.title}
+                  </h5>
+                </div>
+                <div className="flex items-center h-6">
+                  <svg
+                    onClick={(evt) => toggleStar(evt, item.id)}
+                    className={
+                      'w-6 h-6 ms-1 ' +
+                      (item.isStar ? 'text-yellow-300' : 'text-gray-400 ')
+                    }
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                  >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                </div>
+              </div>
               <p
                 className={
-                  'text-slate-600 leading-normal font-light ' +
+                  'text-slate-600 leading-normal font-light min-h-16 ' +
                   (item.status === TodoStatusConst.DONE ? 'line-through' : '')
                 }
               >
                 {item.content}
               </p>
-
-              <div className="flex items-center">
-                <input
-                  checked={item.status === TodoStatusConst.DONE}
-                  id="checked-checkbox"
-                  type="checkbox"
-                  onChange={(evt) => toggleDone(evt, item.id)}
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  htmlFor="checked-checkbox"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Checked state
-                </label>
-              </div>
-              <div className="flex items-center h-6">
-                <svg
-                  onClick={(evt) => toggleStar(evt, item.id)}
-                  className={
-                    'w-6 h-6 ms-1 ' +
-                    (item.isStar ? 'text-yellow-300' : 'text-gray-400 ')
-                  }
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-              </div>
             </div>
           </div>
         ))}
